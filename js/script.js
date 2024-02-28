d7753c918180258e01b771a85183f0c5388bcb8f
Asm Shuvo <shuvo.sust.cse@gmail.com>
2024-02-29 02:52:29 +0600

    blocked text are working

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 
 let tags = '';
 let f=0;
+let blockedText='';
 
 const createJokes = async(tags)=>{
-    const res = await fetch(`https://v2.jokeapi.dev/joke/Any`);
+    const url = `https://v2.jokeapi.dev/joke/${tags}${blockedText=="?blacklistFlags=" ? "":blockedText}`;
+    console.log(url);
+    const res = await fetch(url);
     const data =await res.json();
     console.log("tags: "+tags);
     
@@ -37,32 +40,71 @@ const generateJokes = ()=>{
     else
         createJokes(tags);
 }
-const tagsList = [];
+// const tagsList = [];
 
-const AddTag = (checkbox, tag) => {
-    console.log(checkbox.checked);
-    if (tagsList.includes(tag)) {
-        return;
-    }
+// const AddTag = (checkbox, tag) => {
+//     // console.log(checkbox.checked);
+//     if (tagsList.includes(tag)) {
+//         return;
+//     }
 
-    if (checkbox.checked) {
-        if (f) {
-            tags += ',';
-        } else {
-            f++;
+//     if (checkbox.checked) {
+//         if (f) {
+//             tags += ',';
+//         } else {
+//             f++;
+//         }
+//         tags += tag;
+//         tagsList.push(tag);
+//     } 
+//     if(!checkbox.checked) {
+//         const index = tagsList.indexOf(tag);
+//         if (index !== -1) {
+//             tagsList.splice(index, 1); // Remove tag from tagsList array
+//         }
+//         // Update tags string by joining tagsList array
+//         tags = tagsList.join(',');
+//     }
+
+//     console.log(tags);
+//     // createJokes(tags);
+// };
+
+
+const blockList = [];
+
+const blockTags = (checkbox, tag)=>{
+// https://v2.jokeapi.dev/joke/Any?blacklistFlags=religious,racist
+    if(checkbox.checked){
+        if(!blockList.includes(tag)){
+            blockList.push(tag);
         }
-        tags += tag;
-        tagsList.push(tag);
-    } 
-    if(!checkbox.checked) {
-        const index = tagsList.indexOf(tag);
-        if (index !== -1) {
-            tagsList.splice(index, 1); // Remove tag from tagsList array
-        }
-        // Update tags string by joining tagsList array
-        tags = tagsList.join(',');
     }
-
-    console.log(tags);
-    // createJokes(tags);
-};
+    else{
+        // console.log("else called");
+        
+        if(blockList.includes(tag)){
+            const index = blockList.indexOf(tag);
+            // console.log(index);
+            if (index !== -1) {
+                blockList.splice(index, 1); // Remove tag from blockList array
+            }
+        }
+        // console.log(blockList.length);
+    }
+    if(blockList.length>=0){
+        blockedText='';
+        blockedText+="?blacklistFlags=";
+    }
+    for(let i=0; i<blockList.length; i++){
+        blockedText+=blockList[i];
+        if(i!=blockList.length-1)
+        {
+            blockedText+=",";
+        }
+    }
+    
+    console.log(blockedText);
+    
+    
+}
